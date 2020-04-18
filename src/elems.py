29b08d7640f5dc32038513858b5058576be2e8eb
asrom <asromahin@yandex.ru>
2020-04-18 12:55:52 +0700

    add data class

--- src/elems.py
+++ src/elems.py
@@ -119,7 +119,7 @@ class UniTable(UniElem):
 
 class UniData(UniElem):
     def __init__(self, wd, elem):
-        super().__init__(wd, isreinit_elem=True)
+        super().__init__(wd, elem=elem, isreinit_elem=True)
         self.elem = elem
 
     def reinit(self):
