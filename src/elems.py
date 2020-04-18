ba62e946b3705ee2e9e37d186425be62d669f8cb
asrom <asromahin@yandex.ru>
2020-04-18 12:32:10 +0700

    add data class

--- src/elems.py
+++ src/elems.py
@@ -43,6 +43,12 @@ class UniElem():
                 self.elem = elem
                 break
 
+    def save_as_screen(self, screen_name):
+        if(self.elem):
+            self.wd.execute_script("arguments[0].scrollIntoView();", self.elem)
+            self.wd.save_screenshot(screen_name)
+
+
 
 class UniPaginator(UniElem):
 
